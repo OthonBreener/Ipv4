5d2928bc2ea815ed952c50a11f11289918eaede7
othonbreener <othon.breener@inovamind.com.br>
2021-09-18 21:27:22 -0300

    Função para calculo de subrede

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 #binario = str(input("Entre com o ip da sua rede em binário: "))
-decimal = str(input("Entre com o ip da sua rede em decimal: "))
+#decimal = str(input("Entre com o ip da sua rede em decimal: "))
 #subrede = str(input("Digite quantos bits possuem a subrede: "))
-
 
 def conversion_decimal_for_binario(decimal):
 
@@ -36,7 +35,55 @@ def conversion_decimal_for_binario(decimal):
         number_binario.append(''.join(foobar))
 
     return ('.'.join(number_binario))
-    #'00001010.00010100.00001100.00101101'
+
+def calculo_de_sub_rede(binario, subrede):
+
+    lista_binarios = binario.split('.')
+    number_bits = len(lista_binarios[0]) * 4
+    number_bits_disponiveis = number_bits - subrede
+    hosts = 2 ** number_bits_disponiveis - 2
+
+    total_bits = 32
+    vetor_total_bits = np.arange(total_bits)
+    vetor_subrede = [1 for i in range(subrede)]
+
+    a = len(vetor_subrede)
+    b = len(vetor_total_bits)
+
+    vetor_subrede_auxiliar = vetor_subrede.copy()
+    while  a < b:
+        vetor_subrede_auxiliar.append(0)
+        a = a + 1
+
+    n = 4
+    vetor_subrede_splited = []
+
+    for i in range(total_bits):
+
+        if len(vetor_subrede_splited) == 4:
+            break
+
+        start = int(i * (total_bits / n))
+        end = int((i+1) * (total_bits / n))
+        vetor_subrede_splited.append(vetor_subrede_auxiliar[start:end])
+
+    vetor_subrede_splited_string = []
+    for i in vetor_subrede_splited:
+
+        vetor_subrede_splited_string.append([str(j) for j in i])
+
+    binario_subrede = []
+    for vector in range(len(vetor_subrede_splited_string)):
+
+        a = ''.join(vetor_subrede_splited_string[vector])
+        binario_subrede.append(a)
+
+    binario_subrede = '.'.join(binario_subrede)
+
+    ip_subrede_decimal = conversion_binario_for_decimal(binario_subrede)
+
+    return binario_subrede, ip_subrede_decimal
+
 
 
 def conversion_binario_for_decimal(binario):
@@ -67,13 +114,10 @@ def conversion_binario_for_decimal(binario):
 
     return ('.'.join(conversion_bi))
 
+binario = '00001010.00010100.00001100.00101101'
+subrede = 26
 
-def calculo_de_sub_rede(subrede):
-
-    bi = '1010.10100.1100.101101'
-
-
-
-conversion = conversion_decimal_for_binario(decimal)
+#conversion = conversion_decimal_for_binario(decimal)
+conversion = calculo_de_sub_rede(binario, subrede)
 #conversion_bi = conversion_binario_for_decimal(binario)
 print(conversion)
