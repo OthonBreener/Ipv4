d11b0cc5b7b03203c074e436d480b15137f8e625
othonbreener <othon.breener@inovamind.com.br>
2021-09-26 21:46:12 -0300

    Finalizado tests para primeira função

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,6 @@ from pytest import mark, raises
 
 class TestFunctionConversionDecimalForBinario:
 
-
     def testando_se_o_ip_for_nulo_deve_retornar_mensagem_esperada(self):
 
         params = None
@@ -14,11 +13,11 @@ class TestFunctionConversionDecimalForBinario:
 
         assert conversion_decimal_for_binario(params) == mensagem_esperada
 
-
-    def testando_se_o_ip_for_apenas_o_numero_um_deve_retornar_resultado_esperado(self):
-
-        params = '1'
-        resultado_esperado = '00000001'
+    @mark.parametrize(
+        'params, resultado_esperado',
+        [('1','00000001'),('0','00000000'),('5','00000101')]
+    )
+    def testando_se_o_ip_for_apenas_um_numero_deve_retornar_resultado_esperado(self, params, resultado_esperado):
 
         assert conversion_decimal_for_binario(params) == resultado_esperado
 
@@ -37,11 +36,10 @@ class TestFunctionConversionDecimalForBinario:
 
         assert conversion_decimal_for_binario(params) == resultado_esperado
 
-    @mark.skip(reason = "Ainda não implementei")
+    @mark.task
     def testando_se_o_ip_for_composto_por_decimais_de_tres_digitos_deve_retornar_resultado_esperado(self):
 
         params = '177.69.128.1'
-        resultado_esperado = ''
+        resultado_esperado = '10110001.01000101.10000000.00000001'
 
-        import ipdb; ipdb.set_trace()
-        conversion_decimal_for_binario(params)
+        assert conversion_decimal_for_binario(params) == resultado_esperado
